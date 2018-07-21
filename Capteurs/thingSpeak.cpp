b29bb1be04aade3f690080149da40e63476bad4e
philippeSimier <philaure@wanadoo.fr>
2018-07-21 22:08:23 +0200

    update

--- Capteurs/thingSpeak.cpp
+++ Capteurs/thingSpeak.cpp
@@ -7,7 +7,7 @@
     @version  v1.0 - First release
     @detail   Prérequis    : apt-get update
                              apt-get install libcurl4-openssl-dev
-              Compilation  : g++  thingSpeak.cpp HTTPDownloader.cpp SimpleIni.cpp hx711.cpp spi.c  -lcurl -o thingSpeak
+              Compilation  : g++  thingSpeak.cpp HTTPDownloader.cpp SimpleIni.cpp i2c.cpp  bme280.cpp hx711.cpp spi.c  -lcurl -o thingSpeak
               Execution    : ./main
 */
 
@@ -19,6 +19,7 @@
 #include <iomanip>
 
 #include "hx711.h"
+#include "bme280.h"
 #include "SimpleIni.h"
 #include "HTTPDownloader.h"
 
@@ -29,6 +30,7 @@ using namespace std;
 int main()
 {
     hx711 balance;
+    bme280 capteur(0x77);
     SimpleIni ini;
     HTTPDownloader downloader;
     ostringstream url;
@@ -45,9 +47,15 @@ int main()
     balance.fixerOffset( ini.GetValue<int>("balance", "offset", 0));
     balance.configurerGain(  ini.GetValue<int>("balance", "gain", 128));
 
+    // Configuration du capteur de pression
+    capteur.donnerAltitude( ini.GetValue<float>("ruche", "altitude", 0.0 ));
     string key = ini.GetValue<string>("thingSpeak", "key", " ");
-    url << "https://api.thingspeak.com/update?api_key=" << key << "&field1=" << balance.obtenirPoids();
-    cout << url.str() << endl;
+    url << "https://api.thingspeak.com/update?api_key=" << key;
+    url << "&field1=" << balance.obtenirPoids();
+    url << "&field2=" << capteur.obtenirTemperatureEnC();
+    url << "&field3=" << capteur.obtenirPression0();
+    url << "&field4=" << capteur.obtenirHumidite();
+    // cout << url.str() << endl;
 
     string contenu = downloader.obtenirUrl(url.str());
     cout << contenu << endl;
--- html/definition.inc.php
+++ html/definition.inc.php
@@ -1,12 +1,8 @@
 <?PHP
 
     // Ce Fichier contient la definition des constantes
-	// définition du fichier de configuration.ini
+	// dÃ©finition du fichier de configuration.ini
 
-	define ("CONFIGURATION","/home/pi/Capteurs_I2C/HX711/configuration.ini");
-    
-    //define ("CONFIGURATION","/home/pi/configuration.ini");
-  
-
+	define ("CONFIGURATION","/home/pi/Ruche/configuration.ini");
 
 ?>
