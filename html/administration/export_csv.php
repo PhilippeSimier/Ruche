73c6fb6db65d67a669e967a3b9be7b7a0ac4832f
philippeSimier <philaure@wanadoo.fr>
2018-09-15 21:55:47 +0200

    update export CSV & info systeme

--- html/administration/export_csv.php
+++ html/administration/export_csv.php
@@ -1,6 +1,6 @@
 <?php
 include "authentification/authcheck.php" ;
-	
+
 
 require_once('../ini/ini.php');
 require_once('../definition.inc.php');
@@ -17,29 +17,29 @@ else{
 	$host   = $ini['BDlocale']['host'];
 	$user   = $ini['BDlocale']['user'];
 	$passwd = $ini['BDlocale']['passwd'];
-	$bdd    = $ini['BDlocale']['bdd'];	
-	
+	$bdd    = $ini['BDlocale']['bdd'];
+
 }
 
 // connexion au serveur de la base de données
 
 
-	try 
+	try
 	{
 		$bdd = new PDO('mysql:host=' . $host . ';dbname=' . $bdd, $user, $passwd);
-	} 
-	catch (Exception $ex) 
+	}
+	catch (Exception $ex)
 	{
 		die('<br />Pb connexion serveur BD : ' . $ex->getMessage());
 	}
-	
-	
-	$sql = "SELECT * FROM `mesures` WHERE `id_channel`=" . $ini['ruche']['id'];
+
+
+	$sql = "SELECT * FROM `feeds` WHERE `id_channel`=" . $ini['ruche']['id'];
     $nom_fichier = "channel_". $ini['ruche']['id'] . ".csv";
 
     header('Content-Type: application/csv-tab-delimited-table');
     header('Content-Disposition:attachment;filename='.$nom_fichier);
-	
+
 	$stmt = $bdd->query($sql);
 	while ($data = $stmt->fetchObject()){
 			echo $data->date.",";
@@ -47,7 +47,10 @@ else{
 			echo $data->field2.",";
 			echo $data->field3.",";
 			echo $data->field4.",";
-			echo $data->field5."\n";
+			echo $data->field5.",";
+			echo $data->field6.",";
+			echo $data->field7.",";
+			echo $data->field8."\n";
 	}
 
 
