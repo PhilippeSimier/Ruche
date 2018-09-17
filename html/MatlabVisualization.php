bd6363d4b075369ee03af808a98931d6498a0e7a
philippeSimier <philaure@wanadoo.fr>
2018-09-17 10:37:38 +0200

    update MatlabVisualization

--- html/MatlabVisualization.php
+++ html/MatlabVisualization.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-<?php 
+<?php
     session_start();
 	require_once('definition.inc.php');
     $ini  = parse_ini_file(CONFIGURATION, true);
@@ -12,7 +12,7 @@
 	<!-- Bootstrap CSS version 4.1.1 -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script src="/scripts/bootstrap.min.js"></script> 
+	<script src="/scripts/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/ruche.css" />
 	<title>Matlab Visualization</title>
 </head>
@@ -20,21 +20,24 @@
 	<?php require_once 'menu.php'; ?>
 	<div class="container" style="padding-top: 56px;">
 		<div class="row">
-			<div class="col-md-4 col-sm-12 col-xs-12">
+			<div class="col-md-12 col-sm-12 col-xs-12">
 			    <div class="popin">
-			        <h3>Graphique de dispersion</h3>
-					<p>Les graphiques de dispersion sont utiles pour vérifier l'existance de corrélation entre deux grandeurs physiques. </p>
-					<p>Plus le nuage de points est plat et se groupe autour d'une ligne, plus la corrélation est forte. Plus le nuage de points est circulaire ou éparpillé moins la corrélation est probable.</p>
-				</div>
-			</div>
-			<div class="col-md-8 col-sm-12 col-xs-12">
-			    <div class="popin">
-			    <iframe  height="420" width="100%" style="border: 1px solid #cccccc;" src="
-				<?php echo "https://thingspeak.com/apps/matlab_visualizations/" . $_GET['id'] . "?height=400" ?>
+			    <iframe  height="620" width="100%" style="border: 1px solid #cccccc;" src="
+				<?php echo "https://thingspeak.com/apps/matlab_visualizations/" . $_GET['id'] . "?height=600" ?>
 				">
 				</iframe>
 				</div>
 			</div>
+		</div>
+                <div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="popin">
+                                <h3>Graphique de dispersion</h3>
+                                        <p>Les graphiques de dispersion sont utiles pour vérifier l'existance de corrélation entre deux grandeurs physiques. </p>
+                                        <p>Plus le nuage de points est plat et se groupe autour d'une ligne, plus la corrélation est forte. Plus le nuage de points est circulaire ou éparpillé moins la corrélation est probable.</p>
+                                </div>
+                        </div>
+
 		</div>
 	<?php require_once 'piedDePage.php'; ?>
 </body>
