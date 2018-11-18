80fc7bca59917519673b2a7e5c22708363c1356d
philippeSimier <philaure@wanadoo.fr>
2018-11-18 18:25:46 +0100

    add envoyerURL.cpp

---
+++ Capteurs/envoyerURL.cpp
@@ -0,0 +1,59 @@
+/*!
+    @file     envoyerURL.cpp
+    @author   Philippe SIMIER (Touchard Wahington le Mans)
+    @license  BSD (see license.txt)
+    @brief    Programme pour envoyer une requête en http -> thingSpeak
+	      trois essais sont effectués si le code de retour est différent de 200
+    @date     Octobre 2018
+    @version  v1.0 - First release
+    @detail   compilation g++ envoyerURL.cpp rest.cpp -lcurl -o envoyerURL
+*/
+
+#include <iostream>
+#include "rest.h"
+#include <string.h>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <ctime>
+#include <unistd.h>
+
+
+using namespace std;
+
+/**
+ * @brief ObtenirDateHeure
+ * @return std::string
+ * @details retourne une chaine de caratères représentant la date courante
+ *          au format Année-mois-jour heure:minute:seconde
+
+string ObtenirDateHeure()
+{
+    time_t  t = time(NULL);
+    stringstream ss;
+    ss  <<  put_time( localtime(&t), "%F %T" );
+    return ss.str();
+}
+*/
+
+
+int main(){
+
+    rest requete;
+    string req;
+    int count = 0;
+
+    cin >> req;
+
+    long code = requete.get(req);
+
+    while (code != 200 && count < 2){
+	cout << " envoyerURL : " << requete.getErreurServeur(code) << endl;
+        sleep(30);   // attente de 30s avant nouvel essai
+        code = requete.get(req);
+	count++;
+    }
+    cout << " envoyerURL : " << requete.getErreurServeur(code) << endl;
+    return 0;
+
+}
