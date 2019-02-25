03b3be11310db2724db478c837f39919fdfdc68a
philippeSimier <philaure@wanadoo.fr>
2019-02-25 22:14:09 +0100

    update SQL data

--- SQL/data.sql
+++ SQL/data.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:3306
--- Généré le :  Jeu 13 Septembre 2018 à 16:33
--- Version du serveur :  10.1.26-MariaDB-0+deb9u1
--- Version de PHP :  7.0.27-0+deb9u1
+-- Généré le :  Lun 25 Février 2019 à 17:42
+-- Version du serveur :  10.1.37-MariaDB-0+deb9u1
+-- Version de PHP :  7.0.33-0+deb9u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `data`
+-- Base de données :  `ruche`
 --
 
 -- --------------------------------------------------------
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `channels` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
   `field1` varchar(50) NOT NULL,
   `field2` varchar(50) NOT NULL,
   `field3` varchar(50) NOT NULL,
@@ -36,15 +36,16 @@ CREATE TABLE `channels` (
   `field5` varchar(50) NOT NULL,
   `field6` varchar(50) NOT NULL,
   `field7` varchar(50) NOT NULL,
-  `field8` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `field8` varchar(50) NOT NULL,
+  `statut` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Contenu de la table `channels`
 --
 
-INSERT INTO `channels` (`id`, `name`, `field1`, `field2`, `field3`, `field4`, `field5`, `field6`, `field7`, `field8`) VALUES
-(1, 'Beehive', 'Weight (kg)', 'Temperature (°C)', 'Pressure (kPa)', 'Humidity (%)', 'Illuminance (lux)', 'Dew point (°C)', '', '');
+INSERT INTO `channels` (`id`, `nom`, `field1`, `field2`, `field3`, `field4`, `field5`, `field6`, `field7`, `field8`, `statut`) VALUES
+(566173, 'Ruche Touchard 1', 'Weight (kg)', 'Temperature (°C)', 'Pressure (kPa)', 'Humidity (%)', 'Illuminance (lux)', 'Dew point (°C)', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -54,6 +55,7 @@ INSERT INTO `channels` (`id`, `name`, `field1`, `field2`, `field3`, `field4`, `f
 
 CREATE TABLE `feeds` (
   `id` int(11) NOT NULL,
+  `id_channel` int(11) NOT NULL,
   `field1` float DEFAULT NULL,
   `field2` float DEFAULT NULL,
   `field3` float DEFAULT NULL,
@@ -63,12 +65,71 @@ CREATE TABLE `feeds` (
   `field7` float DEFAULT NULL,
   `field8` float DEFAULT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `id_channel` int(11) NOT NULL,
   `status` varchar(255) NOT NULL,
   `latitude` double NOT NULL,
   `longitude` double NOT NULL,
   `elevation` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `things`
+--
+
+CREATE TABLE `things` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `elevation` float NOT NULL,
+  `nom` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Contenu de la table `things`
+--
+
+INSERT INTO `things` (`id`, `id_user`, `latitude`, `longitude`, `elevation`, `nom`) VALUES
+(1, 1, 47.9957, 0.204413, 50, 'ruche1');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `things_channels`
+--
+
+CREATE TABLE `things_channels` (
+  `id` int(11) NOT NULL,
+  `id_objet` int(11) NOT NULL,
+  `id_canal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Contenu de la table `things_channels`
+--
+
+INSERT INTO `things_channels` (`id`, `id_objet`, `id_canal`) VALUES
+(1, 1, 566173);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(25) NOT NULL,
+  `mdp` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `mdp`) VALUES
+(1, 'Herisson', 'toto');
 
 --
 -- Index pour les tables exportées
@@ -85,7 +146,28 @@ ALTER TABLE `channels`
 --
 ALTER TABLE `feeds`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `channel` (`id_channel`);
+  ADD KEY `canal` (`id_channel`);
+
+--
+-- Index pour la table `things`
+--
+ALTER TABLE `things`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Index pour la table `things_channels`
+--
+ALTER TABLE `things_channels`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_objet` (`id_objet`),
+  ADD KEY `id_canal` (`id_canal`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -95,12 +177,50 @@ ALTER TABLE `feeds`
 -- AUTO_INCREMENT pour la table `channels`
 --
 ALTER TABLE `channels`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=566174;
 --
 -- AUTO_INCREMENT pour la table `feeds`
 --
 ALTER TABLE `feeds`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=483;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8410;
+--
+-- AUTO_INCREMENT pour la table `things`
+--
+ALTER TABLE `things`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `things_channels`
+--
+ALTER TABLE `things_channels`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `feeds`
+--
+ALTER TABLE `feeds`
+  ADD CONSTRAINT `feeds_ibfk_1` FOREIGN KEY (`id_channel`) REFERENCES `channels` (`id`);
+
+--
+-- Contraintes pour la table `things`
+--
+ALTER TABLE `things`
+  ADD CONSTRAINT `things_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `things_channels`
+--
+ALTER TABLE `things_channels`
+  ADD CONSTRAINT `things_channels_ibfk_1` FOREIGN KEY (`id_objet`) REFERENCES `things` (`id`),
+  ADD CONSTRAINT `things_channels_ibfk_2` FOREIGN KEY (`id_canal`) REFERENCES `channels` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
