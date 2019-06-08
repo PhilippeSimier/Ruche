36aeac118342c4e081128a1db89e5cb449b640b6
philippeSimier <philaure@wanadoo.fr>
2019-06-08 09:35:30 +0200

    add webcam

--- Video/webcam.sh
+++ Video/webcam.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-raspistill -a 4 -a "%d %B %Y %X" -ae 32,0xFF,0x000080 -t 1000 -w 1296 -h 972 -o /home/pi/video/cam.jpg
-scp /home/pi/video/cam.jpg root@172.18.58.9:/var/www/Ruche/video
-sleep 18
-raspistill -a 4 -a "%d %B %Y %X" -ae 32,0x00,0xFFFF80 -t 1000 -w 1296 -h 972 -o /home/pi/video/cam.jpg
-scp /home/pi/video/cam.jpg root@172.18.58.9:/var/www/Ruche/video
-sleep 18
-raspistill -a 4 -a "%d %B %Y %X" -ae 32,0xFF,0xFFFF80 -t 1000 -w 1296 -h 972 -o /home/pi/video/cam.jpg
-scp /home/pi/video/cam.jpg root@172.18.58.9:/var/www/Ruche/video
+OUTPUT=/var/tmp/ram_drive
+OPTION='-a 4 -a "%d %B %Y %X" -ae 32,0xFF,0x000080 -t 1000 -w 1296 -h 972'
+
+for i in `seq 1 19 `
+do
+  echo "image $i"
+  raspistill -a 4 -a "%d %B %Y %X" -ae 32,0xFF,0x000080 -t 1000 -w 1296 -h 972  -o $OUTPUT/cam.jpg
+  scp $OUTPUT/cam.jpg root@172.18.58.9:/var/www/Ruche/video
+  sleep 1
+done
+
