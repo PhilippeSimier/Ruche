8abd9d563908491ee1898e8d1c5e2b57e88b6b6c
philippe SIMIER <philaure@wanadoo.fr>
2020-06-09 19:38:44 +0200

    Update webcam.sh

--- Video/webcam.sh
+++ Video/webcam.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-OUTPUT=/var/tmp/ram_drive
-
-for i in $(seq 1 19)
+OUTPUT=/var/tmp/ram_drive/cam.jpg
+OPTIONS='-t 1000 -w 1296 -h 972 -q 10 --exif none'
+unalias cp
+for i in $(seq 1 30)
 do
-  raspistill -a 4 -a "%d %B %Y %X" -ae 32,0xFF,0x000080 -t 1000 -w 1296 -h 972 -o "${OUTPUT}"/cam.jpg
-  scp "${OUTPUT}"/cam.jpg root@172.18.58.9:/var/www/Ruche/video
-  sleep 1
+  raspistill -a 4 -a "%d %B %Y %X" -ae 32,0xFF,0x000080 $OPTIONS -o $OUTPUT
+#  scp $OUTPUT root@172.18.58.9:/var/www/Ruche/video
+  cp -f $OUTPUT /home/pi/video/video
 done
+
 
 
